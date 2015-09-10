d750a9b20836bb3ef63ab02a2f1c1db7366359d7
jlavall <jlavall@clemson.edu>
2015-09-10 14:05:00 -0400

    Trying to color
    JML

--- 01_Sept_Code/Post_Process.m
+++ 01_Sept_Code/Post_Process.m
@@ -37,35 +37,27 @@ load config_XFMRNAMES.mat
 %sort_Results = sortrows(RESULTS(1:10000,1:6),1);
 sort_Results = xlsread('RESULTS_SORTED.xlsx','9_10');
 
-%%
-n = 0;
+%
+n = 100;
 DONE = 0;
 jj = 1;
 ii=1;
-while ii<length(sort_Results)-200
-    while n < 10100
-        if RESULTS(ii,1) == n && DONE == 0
-            %Obtain group of 201simresults:
-            SM.(['PU_',num2str(n)]) = sort_Results(ii:ii+200,3);
-            SM.(['THRM_',num2str(n)]) = sort_Results(ii:ii+200,4);
-            %Now lets sort by desired field.
-            SM.(['PU_',num2str(n)]) = sort(SM.(['PU_',num2str(n)])(:,1));
-            SM.(['THRM_',num2str(n)]) = sort(SM.(['THRM_',num2str(n)])(:,1));
-            
-            %Force kick out of while loop:
-            n = 10100; 
-            DONE = 1;
-            %SM.(pv_size{n+1,1}).A(m,1) = sort_RESULTS(ii,4);
-        end
-        
-        %Make new struct at different installed capacity 
-        n = n + 100;
-    end
-    ii = ii + 1;
-    n = 0;
-    DONE = 0;
+%while ii<length(sort_Results)-200
+while ii < length(sort_Results)
+    %Obtain group of 201simresults:
+    SM.(['PU_',num2str(n)]) = sort_Results(ii:ii+199,2); %1:200 then 201:400
+    SM.(['THRM_',num2str(n)]) = sort_Results(ii:ii+199,4);
+    %
+    %Now lets sort by desired field.
+    SM.(['PU_',num2str(n)]) = sort(SM.(['PU_',num2str(n)])(:,1));
+    SM.(['THRM_',num2str(n)]) = sort(SM.(['THRM_',num2str(n)])(:,1));
+    fprintf('hit!!\n');
+    ii = ii + 200;
+    n = n + 100;
+    %SM.(pv_size{n+1,1}).A(m,1) = sort_RESULTS(ii,4);
 end
-%%
+
+%
 %The object of this while loop is to calculate the interquat
 %
 %Worker variables:
@@ -114,7 +106,7 @@ end
 
 
 
-%%
+%
 %Visualize the results:
 % PLOT!!!
 pv_size = zeros(101,9);
@@ -130,30 +122,100 @@ while n<10100
 end
 %
 %Declare color RGB:
-COLOR= zeros(6,3);
-COLOR(1,:) = [0.0 1.0 1.0];
-COLOR(2,:) = [0.0 0.8 1.0];
-COLOR(3,:) = [0.0 0.6 1.0];
-COLOR(4,:) = [0.0 0.0 0.4]; %inner quartile
-COLOR(5,:) = [0.0 0.0 0.4];
-COLOR(6,:) = [0.0 0.6 1.0];
+COLOR= zeros(9,3);
+COLOR(1,:) = [0.0 1.0 1.0]; %below
+COLOR(2,:) = [0.0 0.8 1.0]; %5th
+COLOR(3,:) = [0.0 0.6 1.0]; %10th
+COLOR(4,:) = [0.0 0.0 0.6]; %25th
+COLOR(5,:) = [1.0 0.0 0.2]; %Median--
+COLOR(6,:) = [0.0 0.0 0.6]; %50th
+COLOR(7,:) = [0.0 0.6 1.0]; %75th
+COLOR(8,:) = [0.0 0.8 1.0]; %90th
+COLOR(9,:) = [0.0 1.0 1.0]; %95th & above
 
-%%
+%
 % "Effect of PV Size on max BUS VOLTAGE under 50% Load"
 fig = 1;
 figure(fig);
+fprintf('before plot\n');
+C = zeros(1,3);
+y2 = zeros(200,1);
+x2 = zeros(200,1);
+%{
+j = 1;
+while j < 10
+    C = COLOR(j,:);
+    
+    if j ~= 5
+        y = Q_V(1:100,j);
+        x = pv_size(2:101,1);
+        fprintf('hit\n');
+        %scatter(pv_size(1:101,j),Q_V(1:101,j))
+        plot(x,y,'Color',C);
+        hold on
+        j = j + 1;
+        %~~~~~~~~~~~~~~~~~
+        y1 = Q_V(1:100,j);
+        plot(x,y1,'Color',C);
+        hold on
+        j = j + 1;
 
-C = zeros(3,1);
-for j=1:1:9
-    %C = COLOR(j,:);
-    %y = Q_V(2:51,j);
-    scatter(pv_size(1:101,j),Q_V(1:101,j))
-    %plot(pv_size(2:51,1),y,'Color',C);
-    hold on
+
+        %Join horizontal vectors together--
+        y2(1:100,1)=y(:,1);
+        y2(101:200,1)=y1(:,1);
+        x2(1:100,1)=x(:,1);
+        x2(101:200,1)=x(:,1);
+
+        fill(x2,y2,C,'EdgeColor','none');
+    else
+        %Only for Median--
+        y = Q_V(1:100,j);
+        x = pv_size(2:101,1);
+        plot(x,y,'Color',C,'LineWidth',4);
+        hold on
+        j = j + 1;
+    end 
 end
+%}
+%Hardcorde way:
+
+%~~~~~~~~~~~~~~~~~
+j = 1;
+C = COLOR(j,:);
+y = Q_V(1:100,j);
+x = pv_size(2:101,1);
+plot(x,y,'Color',C);
+hold on
+j = j + 1;
+%~~~~~~~~~~~~~~~~~
+y1 = Q_V(1:100,4);
+plot(x,y1,'Color',C);
+hold on
+%Join horizontal vectors together--
+%{
+y2(1:100,1)=y(:,1);
+y2(101:200,1)=y1(:,1);
+x2(1:100,1)=x(:,1);
+x2(101:200,1)=x(:,1);
+%}
+%X=zeros(
+X=[x,fliplr(x)];
+x3=10000:100:15000
+X(:,3)=x3;
+Y=[y,fliplr(y1)];
+y3=y(100,1):.00001:y(100,1)+5e-4;
+Y(:,3)=y3;
+%fill(x2,y2,C,'EdgeColor','none');
+fill(X,Y,C,'EdgeColor','none');
+
+j = j + 1;
 
 
-axis([0 5000 1.01 1.05]);
+
+
+
+axis([0 10000 1.02 1.10]);
 
 %% 
 % "Effect of PV size on max LINE loading under 50% load"
