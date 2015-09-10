42a6816ba2fc0c107e9c72c168f9734eca1f30a6
jlavall1 <jlavall@g.clemson.edu>
2015-09-10 19:05:43 -0400

    Fixed Commonwealth -- Subplots
    Also playing w/ coloring

---
+++ 01_Sept_Code/feeder_subplots.m
@@ -0,0 +1,37 @@
+% prompt = 'Enter file path: ';
+% str = input(prompt,'s');
+clear
+clc
+
+str = '''C:\Users\jlavall\Documents\GitHub\CAPER\03_OpenDSS_Circuits\Commonwealth_Circuit_Opendss\run_master_allocate.DSS''';
+% 1. Start the OpenDSS COM. Needs to be done each time MATLAB is opened     
+[DSSCircObj, DSSText] = DSSStartup; 
+    
+% 2. Compiling the circuit     
+DSSText.command = ['Compile ' str]; 
+
+% 3. Solve the circuit. Call anytime you want the circuit to resolve     
+DSSText.command = 'solve'; 
+
+% 4. Run circuitCheck function to double-check for any errors in the circuit before using the toolbox     
+%warnSt = circuitCheck(DSSCircObj);
+
+DSSCircuit = DSSCircObj.ActiveCircuit;
+Buses=getBusInfo(DSSCircObj);
+Lines=getLineInfo(DSSCircObj);
+
+figure(1);
+subplot(2,2,1);
+plotKWProfile(DSSCircObj);
+%title('kw Profile');
+subplot(2,2,2);
+plotKVARProfile(DSSCircObj,'Only3Phase','on');
+%title('
+subplot(2,2,3);
+plotVoltageProfile(DSSCircObj,'SecondarySystem','off');
+subplot(2,2,4);
+plotAmpProfile(DSSCircObj,'258904005');
+
+
+
+% Lines2=getLineInfo_DJM(DSSCircObj, DSSText);
