3f37de21b4f10135cc50b8c3126ec8949d9d1032
bmaceac <bmaceac@clemson.edu>
2015-09-14 15:49:38 -0400

    added loops to build reference matrix

--- 04_DSCADA/Import_DSCADA.m
+++ 04_DSCADA/Import_DSCADA.m
@@ -26,7 +26,40 @@ load.kVAR.B = RidgeRd (1:n, 16);
 load.kVAR.C = RidgeRd (1:n, 18);
 
 
-%FV = xlsread('DEP_CAPER_FEEDER_LOADS.xlsx', 'feltonsville');
+months = [31,28,31,30,31,30,31,31,30,31,30,31,31,28,31];
 
+ref = zeros(n,5);
+
+M=1;
+D=1;
+H=1;
+MIN=0;
+
+% loop runs through entire set of data
+for i = 1:1:n
+    ref(i,1) = M;       % updates to reference matrix
+    ref(i,2) = D;
+    ref(i,3) = H;
+    ref(i,4) = MIN;
+    
+   MIN=MIN+15;          % increment minute for every 15
+   
+   if MIN ==60
+       MIN = 0;
+       H=H+1;           % increment hour every 60 mins
+       
+       if H==24
+           H=1;
+           D=D+1;       % increment day every 24 hours
+           
+           if D>months(M);
+               D=1;
+               M=M+1;   % increment month 
+           end
+       end
+   end
+end
+
+%FV = xlsread('DEP_CAPER_FEEDER_LOADS.xlsx', 'feltonsville');
 %WilmSt = xlsread('DEP_CAPER_FEEDER_LOADS.xlsx', 'WilmingtonSt');
 
