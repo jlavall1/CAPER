172a5af0b83c991064e2e30e8bc058138718b2d1
Morey-Agnew <magnew@clemson.edu>
2016-03-08 10:34:27 -0500

    lat-long program

--- 10_Momo/lat_long_load.m
+++ 10_Momo/lat_long_load.m
@@ -1,1 +1,32 @@
-% Day 1 Start. March 3
+% Start March 3. Morey Agnew
+
+% Read from excel fil with all lat long coordinates
+% num has only numerical values, txt has only strings, 
+% raw has both but considered strings
+[num,txt,raw] = xlsread('DEC_PV_LIST.xlsm','Carolinas_Substation_Long-lat');
+
+% we only want distribution type substations
+substation_types = raw(:,3);
+% loop variable for setting only distribution substations.
+j = 1;
+% use to check for distrubution types
+dis = 'DISTRIBUTION';
+
+% 2037 because thats how many rows there are
+for i = 1:2037
+    % if strcmp returns true then set values for dis, lat, and lon and
+    % increment j
+    if strcmp(dis,substation_types(i,1))
+        dis_vector(j,:) = raw(i,:);
+        lat_dis(j,1) = num(i-1,4);
+        lon_dis(j,1) = num(i-1,5);
+        j = j + 1;
+    end
+end
+
+%Type in command window to bring up all distribution type points
+%lat = lat_dis;
+%lon = lon_dis;
+%plot(lon,lat,'.r','MarkerSize',20)
+%plotGoogleMap
+
