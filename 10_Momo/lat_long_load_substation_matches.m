7575edb31427804a82eaaa41a2fd42e462ff6c70
Morey-Agnew <magnew@clemson.edu>
2016-03-10 01:52:08 -0500

    comments and fixes to new substation_matches program

--- 10_Momo/lat_long_load_substation_matches.m
+++ 10_Momo/lat_long_load_substation_matches.m
@@ -3,23 +3,35 @@
 
 % read in lat_long_data.xls file
 [num2,~,raw2] = xlsread('Lat_Long_data.xls','Lat_Long_data');
-
+% incrementing variable
 j = 1;
+% find size to determine end for the loop
 [r c] = size(raw2);
 
+% loop to set only the distribution, approved, solar, and carolina types
 for i = 1:r
     if strcmpi(raw2(i,39),'Distribution')
         if strcmpi(raw2(i,48),'Approved')
             if strcmpi(raw2(i,42), 'Solar')
                 if strncmpi(raw2(i,50), 'Car',3)
-                    matrix(j,:) = raw2(i,:);
-                    lat_vector(j,1) = num2(i-1,25);
-                    lon_vector(j,1) = num2(i-1,24);
-                    j = j + 1;
+                    % do this to eliminate the ones we currently 
+                    %   dont have lat long coordinates for 
+                    if ~(num2(i-1,25) == 0) && ~(num2(i-1,24) == 0)
+                        matrix(j,:) = raw2(i,:);
+                        lat_vector(j,1) = num2(i-1,25);
+                        lon_vector(j,1) = num2(i-1,24);
+                        j = j + 1;
+                    end
                 end
             end
         end
     end
 end
+
+% this is to run the plotGoogleMap function.
+lat = lat_vector;
+lon = lon_vector;
+plot(lon,lat,'.r','MarkerSize',20);
+plotGoogleMap;
 
         
--- 10_Momo/lat_long_load_substations.m
+++ 10_Momo/lat_long_load_substations.m
@@ -3,7 +3,7 @@
 % Read from excel fil with all lat long coordinates
 % num has only numerical values, txt has only strings, 
 % raw has both but considered strings
-[num,txt,raw] = xlsread('DEC_PV_LIST.xlsm','Carolinas_Substation_Long-lat');
+[num,~,raw] = xlsread('DEC_PV_LIST.xlsm','Carolinas_Substation_Long-lat');
 
 % we only want distribution type substations
 substation_types = raw(:,3);
@@ -15,7 +15,7 @@ dis = 'DIST';
 % length is how many rows there are
 for i = 1:length(substation_types)
     % if strcmp returns true then set values for dis, lat, and lon and
-    % increment j
+    %   increment j
     if strncmp(substation_types(i,1),dis,4)
         dis_vector(j,:) = raw(i,:);
         lat_dis(j,1) = num(i-1,4);
@@ -24,9 +24,9 @@ for i = 1:length(substation_types)
     end
 end
 
-%copy this in command window to bring up all distribution type points
-%lat = lat_dis;
-%lon = lon_dis;
-%plot(lon,lat,'.r','MarkerSize',20)
-%plotGoogleMap
+%this is used to run the plotGoogleMap function
+lat = lat_dis;
+lon = lon_dis;
+plot(lon,lat,'.r','MarkerSize',20)
+plotGoogleMap
 
