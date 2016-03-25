b90994bfc5a254ab432d734750f28c45ec2398f0
ezanin <ezanin@g.clemson.edu>
2016-03-24 19:57:05 -0500

    lord helo me

--- 08_ezanin/Map_3D.m
+++ 08_ezanin/Map_3D.m
@@ -2,12 +2,12 @@
 clear
 clc
 close all
-%{
+
 [sort_Results,~,cell] = xlsread('Lat_Long_data.xls','Lat_Long_data');
 j = 1;
 for i=2:1:length(cell)
-    if strcmp(cell{i,42},'Solar') == 1 && cell{i,24} ~= 0
-        if isnan(cell{i,55}) == 0 && strcmp(cell{i,39},'Transmission') ~= 1
+    if strcmpi(cell{i,42},'Solar') == 1 && cell{i,24} ~= 0
+        if isnan(cell{i,55}) == 0 && strcmpi(cell{i,39},'Transmission') ~= 1
             DER_PV_SITE(j).X = cell{i,24};
             DER_PV_SITE(j).Y = cell{i,25};
             DER_PV_SITE(j).PPA = cell{i,55};
@@ -15,7 +15,7 @@ for i=2:1:length(cell)
         end
     end
 end
-%}
+
 load DER_PV_SITE.mat
 %%
 
@@ -28,7 +28,7 @@ for j=1:1:length(XX)
     Y_3(j,:)=ones(1,length(YY))*YY(j);
 end
 Z_3=zeros(length(XX),length(YY));
-%{
+
 hit = 0;
 j = 1;
 i = 1;
@@ -50,7 +50,7 @@ for k=1:1:length(ZZ)
     disp(k)
     hit = 0;
 end
-%}
+
 for k=1:1:length(ZZ)
     Z_3(k,k)=Z_3(k,k)+DER_PV_SITE(k).PPA;
 end
